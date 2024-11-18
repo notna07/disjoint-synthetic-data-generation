b6c54a1aa73a1cd34119fd1ceb2b5b655b7f4c79
Anton D. Lautrup <70313789+notna07@users.noreply.github.com>
2024-11-18 15:27:11 +0100

    test cases

---
+++ test.py
@@ -0,0 +1,12 @@
+
+
+from synthcity.plugins import Plugin
+from disjoint_generative_model.disjoint_model import DisjointGenerativeModels
+
+model = Plugin().get('ctgan')
+
+
+
+
+if __name__ == '__main__':
+    pass
